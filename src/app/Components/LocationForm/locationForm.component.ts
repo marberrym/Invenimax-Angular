28d77a0c904580b0873cf776bc1d9e61915e6973
Matthew <marberrym@gmail.com>
2019-01-19 21:39:03 -0500

    Added a drop down state feature to the location submission.

--- src/app/Components/LocationForm/locationForm.component.ts
+++ src/app/Components/LocationForm/locationForm.component.ts
@@ -13,6 +13,13 @@ export class LocationForm {
     model = new Location("", "", "", 0, "", 0);
     submitted = false;
 
+    states = [
+        "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN",
+        "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH",
+        "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
+        "VT", "VA", "WA", "WV", "WI", "WY"
+    ]
+
     ngOnInit() {
     }
 
